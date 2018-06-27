fa0262233c9619e72e0aed1dbfc63a247964f1e0
geoffrey woodcock <theatomproject@gmail.com>
2018-06-27 01:18:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,12 +83,12 @@ class Student
        sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10
+      WHERE grade = ?
       
     SQL
-       DB[:conn].execute(sql).map do |row|
+       DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end 
   
   
