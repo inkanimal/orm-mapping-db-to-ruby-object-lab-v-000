f054d964cfa155e87e04b378d1eed826f19ea02a
geoffrey woodcock <theatomproject@gmail.com>
2018-06-27 01:25:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,12 +55,12 @@ class Student
     end
   end 
   
-  def self.first_x_students_in_grade_10
+  def self.first_x_students_in_grade_10(x)
        sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = 10
-      
+      LIMIT x
     SQL
        DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
