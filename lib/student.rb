095e1c649e97557d79d8871acc46e0e579240959
geoffrey woodcock <theatomproject@gmail.com>
2018-06-27 00:55:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,13 @@ class Student
   end
   
   def self.count_all_students_in_grade_9
-     
+       sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
   end 
   
   
