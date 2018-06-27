498f77197ba07543d1560eceaf7658e19ed361ee
geoffrey woodcock <theatomproject@gmail.com>
2018-06-27 01:43:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
     end
   end 
   
-  def self.first_X_students_in_grade_10(X)
+  def self.first_X_students_in_grade_10(grade)
        sql = <<-SQL
       SELECT *
       FROM students
